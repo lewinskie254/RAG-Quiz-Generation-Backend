b06d420ad6f91b370cee4c0c96b40e40bbc22e90
lewinskie254 <machiulewis@gmail.com>
2025-11-19 17:33:56 +0300

    method to routinely add student details

--- student/views_folder/student_view.py
+++ student/views_folder/student_view.py
@@ -10,6 +10,7 @@ class StudentView(APIView):
     def post(self, request, action, id=None, instance=None):
         actions = {
             "add-student": self.add_new_student,
+            "update-student" : self.update_student, 
         }
         func = actions.get(action)
         if not func:
@@ -56,9 +57,39 @@ class StudentView(APIView):
         else:
             return Response(student_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+    def update_student(self, request, id):
+        student = get_object_or_404(Student, id=id)
+
+        # Optional updates
+        course_id = request.data.get("course")
+        school_id = request.data.get("school")
+
+        course = get_object_or_404(Course, id=course_id) if course_id else student.course
+        school = get_object_or_404(School, id=school_id) if school_id else student.school
+
+        data = {
+            "name": request.data.get("student_name", student.name),
+            "email": request.data.get("email", student.email),
+            "phone_number": request.data.get("phone_number", student.phone_number),
+            "course": course,
+            "school": school,
+            # DON’T override grade unless needed
+            "grade": request.data.get("grade", student.grade),
+        }
+
+        serializer = StudentSerializer(student, data=data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                {"message": f"{data['name']} updated successfully"},
+                status=status.HTTP_200_OK
+            )
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
     def show_student_details(self, request, id, instance=None): 
         student = get_object_or_404(Student, id=id)
         student_serializer = StudentSerializer(student)
         return Response({"student": student_serializer.data}, status=status.HTTP_200_OK)
+    
